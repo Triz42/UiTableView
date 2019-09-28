1fb45bbc328eaa88f2d913b31cb38b043ad53302
Beatriz Teles <beatrizteles868@gmail.com>
2019-09-28 16:55:44 -0300

    Initial Commit

---
+++ estudoDeTable/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  estudoDeTable
+//
+//  Created by Beatriz Teles Castro on 21/09/19.
+//  Copyright © 2019 Beatriz Teles Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let Meals = ["Eggplant", "Zuquini", "Sundubun"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
